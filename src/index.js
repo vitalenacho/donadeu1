2b49817dc5619d76af66fd60635c70f128be8cd4
vitalenacho <59450226+vitalenacho@users.noreply.github.com>
2022-10-03 13:26:38 -0300

    audit de npm

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ require("./lib/passport");
 // Settings (Configuraciones iniciales del sistema)
 app.set('port', process.env.PORT || 4000);
 app.set('views', path.join(__dirname, 'views'));
-app.engine('.hbs', exphbs({
+app.engine('.hbs', exphbs.engine({
     defaultLayout: 'main',
     layoutsDir: path.join(app.get('views'), 'layouts'),
     partialsDir: path.join(app.get('views'), 'partials'),
